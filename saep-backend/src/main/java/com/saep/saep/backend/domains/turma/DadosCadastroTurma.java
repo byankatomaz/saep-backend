1756d0de7e565e8af93d9e31b3cc19691706ed93
byankatomaz <byankatomaz@gmail.com>
2024-06-06 16:57:56 -0300

    feat

---
+++ saep-backend/src/main/java/com/saep/saep/backend/domains/turma/DadosCadastroTurma.java
@@ -0,0 +1,11 @@
+package com.saep.saep.backend.domains.turma;
+
+import com.saep.saep.backend.domain.usuario.Usuario;
+
+public record DadosCadastroTurma(
+
+        String nome,
+        Long professor
+
+) {
+}
